4ed24879d7dac0dbaf59bfeee3fee42976538d6a
Shane Upchurch <shaneupchurch@gmail.com>
2020-10-29 20:33:06 -0500

    connects to API and renders

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react'
-import PokeList from './PokeList.js';
-import SearchBar from './SearchBar.js';
-import Sort from './Sort.js'
+import request from 'superagent';
 
 export default class ListPage extends Component {
-
 
     state = {
         namedPoke: '',
         submittedPokemon: '',
         attackFiltered: '',
         defenseFiltered: '',
+        orderFiltered: '',
+        pokeArray: []
     }
+
+    componentDidMount = async () => {
+        await this.fetchPokemon();
+    }
+
+    handleForm = async (e) => {
+        e.preventDefault()
+        await this.fetchPokemon();
+    }
+
+    fetchPokemon = async () => {
+        const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.namedPoke}`)
+        this.setState({ pokeArray: data.body.results })
+    }
+
+    handleSearchChange = (e) => {
+        this.setState({
+            namedPoke: e.target.value
+        })
+    }
+
+
+
+
 
     handleAttackFilter = (e) => {
         this.setState({
@@ -25,28 +48,35 @@ export default class ListPage extends Component {
         })
     }
 
-    handleSearchChange = (e) => {
-        this.setState({
-            namedPoke: e.target.value
-        })
-    }
 
-    handleForm = (e) => {
-        e.preventDefault()
-        this.setState({
-            submittedPokemon: this.state.namedPoke
-        })
-    }
+
+
 
 
 
     render() {
         return (
             <div>
-                <SearchBar searchFilter={this.handleForm} name={this.handleSearchChange} />
-                <Sort attackFilter={this.handleAttackFilter} defenseFilter={this.handledefenseFilter} />
-                <PokeList namedPoke={this.state.submittedPokemon} attackFiltered={this.state.attackFiltered} defenseFiltered={this.state.defenseFiltered} />
+                <form onSubmit={this.handleForm}>
+                    Looking for someone specific?
+                <input type='text' onChange={this.handleSearchChange} />
+                    <button>Search by Character Name</button>
+                </form>
+                {
+                    this.state.pokeArray.length === 0
+                        ? 'Be Patient, Still Loading'
+                        : this.state.pokeArray.map(poke =>
+                            <div>
+                                <h2>{poke.pokemon}</h2>)
+                                <img src={poke.url_image} alt="" width="100" height="200" />
+                                <p>attack: {poke.attack}</p>
+                                <p>defense: {poke.defense}</p>
+                                <p>ability: {poke.ability_1}</p>
+                            </div>
+                        )
+                }
             </div>
+
         )
     }
 }
